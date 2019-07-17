630ebc3bf51ce435c96e01bc3e58ef27c34cfb79
Ben Langmuir <blangmuir@apple.com>
2019-07-17 14:55:31 -0700

    Factor and improve

--- Tests/SourceKitTests/SourceKitTests.swift
+++ Tests/SourceKitTests/SourceKitTests.swift
@@ -62,21 +62,19 @@ final class SKTests: XCTestCase {
     }
 
   func testIndex() throws {
-    let ws = try skTibsWorkspace(name: "SwiftModules")
+    guard let ws = try staticSourceKitTibsWorkspace(name: "SwiftModules") else { return }
     try ws.buildAndIndex()
 
     let locDef = ws.testLoc("aaa:def")
     let locRef = ws.testLoc("aaa:call:c")
-    ws.sk.send(DidOpenTextDocument(textDocument: TextDocumentItem(
-      url: locRef.url,
-      language: .swift,
-      version: 1,
-      text: try ws.sources.sourceCache.get(locRef.url))))
+
+    try ws.openDocument(locDef.url, language: .swift)
+    try ws.openDocument(locRef.url, language: .swift)
 
     // MARK: Jump to definition
 
     let jump = try ws.sk.sendSync(DefinitionRequest(
-      textDocument: TextDocumentIdentifier(locRef.url),
+      textDocument: locRef.docIdentifier,
       position: locRef.position))
 
     XCTAssertEqual(jump.count, 1)
@@ -85,112 +83,132 @@ final class SKTests: XCTestCase {
 
     // MARK: Find references
 
-    ws.sk.send(DidOpenTextDocument(textDocument: TextDocumentItem(
-      url: locDef.url,
-      language: .swift,
-      version: 1,
-      text: try ws.sources.sourceCache.get(locDef.url))))
-
     let refs = try ws.sk.sendSync(ReferencesRequest(
-      textDocument: TextDocumentIdentifier(locDef.url),
+      textDocument: locDef.docIdentifier,
       position: locDef.position))
 
     XCTAssertEqual(3, refs.count)
   }
-
 }
 
 extension TestLoc {
   public var position: Position {
+    Position(self)
+  }
+}
+
+extension Position {
+  init(_ loc: TestLoc) {
     // FIXME: utf16 vfs utf8 column
-    Position(line: line - 1, utf16index: column - 1)
+    self.init(line: loc.line - 1, utf16index: loc.column - 1)
+  }
+}
+
+extension XCTestCase {
+
+  public func staticSourceKitTibsWorkspace(name: String, testFile: String = #file) throws -> SKTibsWorkspace? {
+    let testDirName = testDirectoryName
+    let workspace = try SKTibsWorkspace(
+      immutableProjectDir: inputsDirectory(testFile: testFile)
+        .appendingPathComponent(name, isDirectory: true),
+      persistentBuildDir: XCTestCase.productsDirectory
+        .appendingPathComponent("sk-tests/\(testDirName)", isDirectory: true),
+      tmpDir: URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        .appendingPathComponent("sk-test-data/\(testDirName)", isDirectory: true),
+      toolchain: ToolchainRegistry.shared.default!)
+
+    if workspace.builder.targets.contains(where: { target in !target.clangTUs.isEmpty })
+      && !workspace.builder.toolchain.clangHasIndexSupport {
+      fputs("warning: skipping test because '\(workspace.builder.toolchain.clang.path)' does not " +
+            "have indexstore support; use swift-clang\n", stderr)
+      return nil
+    }
+
+    return workspace
+  }
+}
+
+
+
+// MARK: - New -
+
+extension TibsToolchain {
+  convenience init(_ sktc: Toolchain) {
+    self.init(
+      swiftc: sktc.swiftc!.asURL,
+      clang: sktc.clang!.asURL,
+      libIndexStore: sktc.libIndexStore!.asURL,
+      tibs: XCTestCase.productsDirectory.appendingPathComponent("tibs", isDirectory: false),
+      ninja: findTool(name: "ninja"))
   }
 }
 
 public final class SKTibsWorkspace {
 
-  public static let defaultToolchain = TibsToolchain(
-    swiftc: findTool(name: "swiftc")!,
-    clang: findTool(name: "clang")!,
-    tibs: XCTestCase.productsDirectory.appendingPathComponent("tibs"),
-    ninja: findTool(name: "ninja"))
+  public let tibsWorkspace: TibsTestWorkspace
+  public let testServer = TestSourceKitServer(connectionKind: .local)
 
-  public var sources: TestSources
-  public var builder: TibsBuilder
-  public let index: IndexStoreDB
-  public let testServer = TestSourceKitServer(connectionKind: .jsonrpc)
-  public let tmpDir: URL
-
-  public init(projectDir: URL, buildDir: URL, tmpDir: URL, toolchain: TibsToolchain = SKTibsWorkspace.defaultToolchain) throws {
-    sources = try TestSources(rootDirectory: projectDir)
-
-    let fm = FileManager.default
-    try fm.createDirectory(at: buildDir, withIntermediateDirectories: true, attributes: nil)
-
-    let manifestURL = projectDir.appendingPathComponent("project.json")
-    let manifest = try JSONDecoder().decode(TibsManifest.self, from: try Data(contentsOf: manifestURL))
-    builder = try TibsBuilder(manifest: manifest, sourceRoot: projectDir, buildRoot: buildDir, toolchain: toolchain)
-
-    try builder.writeBuildFiles()
-    try fm.createDirectory(at: builder.indexstore, withIntermediateDirectories: true, attributes: nil)
-
-    let libIndexStore = try IndexStoreLibrary(dylibPath: toolchain.swiftc
-      .deletingLastPathComponent()
-      .deletingLastPathComponent()
-      .appendingPathComponent("lib")
-      .appendingPathComponent("libIndexStore.dylib")
-      .path) // FIXME: non-Mac
-
-    self.tmpDir = tmpDir
-
-    index = try IndexStoreDB(
-      storePath: builder.indexstore.path,
-      databasePath: tmpDir.path,
-      library: libIndexStore, listenToUnitEvents: false)
-
-    let buildPath = AbsolutePath(buildDir.path)
-
-    testServer.server!.workspace = Workspace(
-          rootPath: AbsolutePath(projectDir.path),
-          clientCapabilities: ClientCapabilities(),
-          buildSettings: CompilationDatabaseBuildSystem(projectRoot: buildPath),
-          index: index,
-          buildSetup: BuildSetup(configuration: .debug, path: buildPath, flags: BuildFlags()))
-
-    sk.allowUnexpectedNotification = true
-  }
-
-  deinit {
-    _ = try? FileManager.default.removeItem(atPath: tmpDir.path)
-  }
-}
-
-extension SKTibsWorkspace {
-
+  public var index: IndexStoreDB { tibsWorkspace.index }
+  public var builder: TibsBuilder { tibsWorkspace.builder }
+  public var sources: TestSources { tibsWorkspace.sources }
   public var sk: TestClient { testServer.client }
 
-  public func buildAndIndex() throws {
-    try builder.build()
-    index.pollForUnitChangesAndWait()
+  public init(
+    immutableProjectDir: URL,
+    persistentBuildDir: URL,
+    tmpDir: URL,
+    toolchain: Toolchain) throws
+  {
+    self.tibsWorkspace = try TibsTestWorkspace(
+      immutableProjectDir: immutableProjectDir,
+      persistentBuildDir: persistentBuildDir,
+      tmpDir: tmpDir,
+      toolchain: TibsToolchain(toolchain))
+
+    sk.allowUnexpectedNotification = true
+    initWorkspace()
+  }
+
+  public init(projectDir: URL, tmpDir: URL, toolchain: Toolchain) throws {
+    self.tibsWorkspace = try TibsTestWorkspace(
+      projectDir: projectDir,
+      tmpDir: tmpDir,
+      toolchain: TibsToolchain(toolchain))
+
+    sk.allowUnexpectedNotification = true
+    initWorkspace()
+  }
+
+  func initWorkspace() {
+    let buildPath = AbsolutePath(builder.buildRoot.path)
+    testServer.server!.workspace = Workspace(
+      rootPath: AbsolutePath(sources.rootDirectory.path),
+      clientCapabilities: ClientCapabilities(),
+      buildSettings: CompilationDatabaseBuildSystem(projectRoot: buildPath),
+      index: index,
+      buildSetup: BuildSetup(configuration: .debug, path: buildPath, flags: BuildFlags()))
   }
 }
 
 extension SKTibsWorkspace {
 
   public func testLoc(_ name: String) -> TestLoc { sources.locations[name]! }
+
+  public func buildAndIndex() throws {
+    try tibsWorkspace.buildAndIndex()
+  }
 }
 
-extension XCTestCase {
-
-  public func skTibsWorkspace(name: String, testFile: String = #file) throws -> SKTibsWorkspace {
-    let testDirName = testDirectoryName
-    return try SKTibsWorkspace(
-      projectDir: inputsDirectory(testFile: testFile).appendingPathComponent(name),
-      buildDir: XCTestCase.productsDirectory
-        .appendingPathComponent("sk-tests")
-        .appendingPathComponent(testDirName),
-      tmpDir: URL(fileURLWithPath: NSTemporaryDirectory())
-        .appendingPathComponent("sk-test-data")
-        .appendingPathComponent(testDirName))
+extension SKTibsWorkspace {
+  func openDocument(_ url: URL, language: Language) throws {
+    sk.send(DidOpenTextDocument(textDocument: TextDocumentItem(
+      url: url,
+      language: language,
+      version: 1,
+      text: try sources.sourceCache.get(url))))
   }
+}
+
+extension TestLoc {
+  var docIdentifier: TextDocumentIdentifier { TextDocumentIdentifier(url) }
 }
